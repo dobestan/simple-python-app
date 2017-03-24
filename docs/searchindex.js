5f365a8bf1dd4c1690bd1bc7a262f43006887cad
Suchan An <me@ansuchan.com>
2017-03-24 15:50:40 +0900

    add html docs

---
+++ docs/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:51,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to Simple Python App&#8217;s documentation!"],titleterms:{app:0,document:0,indic:0,python:0,simpl:0,tabl:0,welcom:0}})
